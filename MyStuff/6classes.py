8b75b418312186f24ae22d30ee78051d8b3c36b4
Niklas Carstensen <niklas.carstensen@outlook.de>
2022-08-21 19:54:26 +0200

    Change more var names

--- MyStuff/6classes.py
+++ MyStuff/6classes.py
@@ -4,12 +4,12 @@ class Actor:
         self.hp = start_hp
         self.damage = 2
     
-    def hit(self, other_enemy):
-        other_enemy.hp -= self.damage
+    def hit(self, other_actor):
+        other_actor.hp -= self.damage
         
-def print_hp(enemy1, enemy2):
-    print(enemy1.name + ": " + str(enemy1.hp))
-    print(enemy2.name + ": " + str(enemy2.hp))
+def print_hp(actor1, actor2):
+    print(actor1.name + ": " + str(actor1.hp))
+    print(actor2.name + ": " + str(actor2.hp))
 
 
 link = Actor("Link", 5)
