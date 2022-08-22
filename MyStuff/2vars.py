c3bf7c39e544bca46f1f26e19d3a3f828c97575b
Niklas Carstensen <niklas.carstensen@outlook.de>
2022-08-22 15:55:28 +0200

    Update vars lecture to include operations

--- MyStuff/2vars.py
+++ MyStuff/2vars.py
@@ -1,14 +1,21 @@
+print("Auswerung")
 x = 2
 x = x + 1
 print(x)
 
+print("Text operationen")
 s = "Hello"
 s = s + "World"
 print(s)
 
-y = x + 0.5
-print(y)
+print("Zahlen operationen")
+print("3 + 4 = " + str(3 + 4))
+print("3 - 4 = " + str(3 - 4))
+print("3 * 4 = " + str(3 * 4))
+print("3 / 4 = " + str(3 / 4))
+print("9 % 4 = " + str(9 % 4))
 
+print("Datenstrukturen")
 a = [3,5,7,9]
 print(a[1])
 print(a)
